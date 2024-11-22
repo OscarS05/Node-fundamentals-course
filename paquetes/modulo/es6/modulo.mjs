33b131a8cdf0519cbeda99fdfbcac30eda2e543c
oscar05 <santiagoM0509@hotmail.com>
2024-11-22 08:06:12 -0500

    require() e import. Construyendo módulos

---
+++ paquetes/modulo/es6/modulo.mjs
@@ -0,0 +1,10 @@
+function saludar(){
+    console.log('Hola mundo');
+}
+
+// export default saludar;
+
+export default {
+    saludar,
+    prop1: "Soy un modulo",
+};
---
+++ paquetes/modulo/index.js
@@ -0,0 +1,6 @@
+// Trae nuestro módulo 
+const modulo = require('./modulo');
+
+// Ejecutar una función del modulo
+console.log(modulo.prop1);
+modulo.saludar();
